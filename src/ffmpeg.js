328a7f2ece1f556bd3bfcba6c86b664a0240db13
Matt Dziuban <mrdziuban@gmail.com>
2017-08-12 22:04:53 -0400

    Use correct ffmpeg binary, fix eslint errors.

--- src/ffmpeg.js
+++ src/ffmpeg.js
@@ -2,7 +2,6 @@ const { path: ffmpegBin } = require('ffmpeg-static');
 const { path: ffprobeBin } = require('ffprobe-static');
 const ProgressPromise = require('progress-promise');
 const progressStream = require('ffmpeg-progress-stream');
-const { spawn } = require('child_process');
 const runCmd = require('./cmd');
 
 // The `replace` call is necessary to use the static binaries in the bundled .asar file
@@ -17,17 +16,17 @@ module.exports = {
         stdout => console.log('FFPROBE STDOUT:', `${stdout}`),
         stderr => console.log('FFPROBE STDERR:', `${stderr}`),
         (code, stdout, stderr) =>
-          code === 0
+          (code === 0
             ? resolve(parseFloat(stdout[0].match(/^duration=([^\n]+)$/m)[1]))
-            : reject(stderr)))
+            : reject(stderr))));
   },
 
   convert(input, output) {
     return new ProgressPromise((resolve, reject, progress) =>
-      runCmd('ffmpeg', ['-i', input, '-vcodec', 'libx264', '-crf', `${window.crf || 18}`, '-y', output],
+      runCmd(ffmpeg, ['-i', input, '-vcodec', 'libx264', '-crf', `${window.crf || 18}`, '-y', output],
         stdout => console.log('FFMPEG STDOUT:', `${stdout}`),
-        stderr => { progress(stderr); console.log('FFMPEG STDERR:', stderr); },
-        (code, stdout, stderr) => (code === 0 ? resolve() : reject()),
+        (stderr) => { progress(stderr); console.log('FFMPEG STDERR:', stderr); },
+        code => (code === 0 ? resolve() : reject()),
         progressStream()));
   }
 };
--- src/util.js
+++ src/util.js
@@ -2,7 +2,7 @@ const timeFns = [
   seconds => seconds,
   minutes => minutes * 60,
   hours => hours * 60 * 60
-]
+];
 
 module.exports = {
   timeStrToSec: time => time.split(':').reverse().reduce((acc, s, i) => acc + timeFns[i](parseFloat(s)), 0)
