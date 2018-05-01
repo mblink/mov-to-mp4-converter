905f40dfa54887d18cc9bbb0ec79def3ba77ddbe
Matt Dziuban <mrdziuban@gmail.com>
2018-05-01 15:29:52 -0400

    Fix ffprobe logic to properly get video stream.

--- src/ffmpeg.js
+++ src/ffmpeg.js
@@ -5,6 +5,7 @@ const ProgressPromise = require('progress-promise');
 const progressStream = require('ffmpeg-progress-stream');
 const runCmd = require('./cmd');
 const util = require('./util');
+const log = require('electron-log');
 
 // The `replace` call is necessary to use the static binaries in the bundled .asar file
 // https://stackoverflow.com/a/43389268/2163024
@@ -20,32 +21,41 @@ module.exports = {
       if (err) {
         errorCb(err);
       } else {
-        const ret = info.streams[0];
-        ret.duration = parseFloat(ret.duration);
-        successCb(ret);
+        const ret = info.streams.find(s => s.codec_type === 'video');
+        if (ret) {
+          ret.duration = parseFloat(ret.duration);
+          successCb(ret);
+        } else {
+          errorCb(new Error('No video stream found in ffprobe response'));
+        }
       }
     });
   },
 
   convert(input, output) {
     return new ProgressPromise((resolve, reject, progress) =>
-      this.getInfo(input, ({ width, height, duration }) => {
+      this.getInfo(input, (info) => {
+        log.info('FFPROBE INFO:', info);
+
         const opts = ['-i', input, '-vcodec', 'libx264', '-crf', `${window.crf || 18}`, '-y'];
-        if (width > maxWidth || height > maxHeight) {
+        if (info.width > maxWidth || info.height > maxHeight) {
           opts.push('-vf');
-          const dims = util.fitToMaxDimensions(width, height, maxWidth, maxHeight);
+          const dims = util.fitToMaxDimensions(info.width, info.height, maxWidth, maxHeight);
           opts.push(`scale=${dims[0]}:${dims[1]}`);
         }
         opts.push(output);
 
-        runCmd(ffmpeg, opts,
+        runCmd(
+          ffmpeg,
+          opts,
           stdout => console.log('FFMPEG STDOUT:', `${stdout}`),
           (stderr) => {
             console.log('FFMPEG STDERR:', stderr);
-            progress((util.timeStrToSec(stderr.time) / duration) * 100);
+            progress((util.timeStrToSec(stderr.time) / info.duration) * 100);
           },
           code => (code === 0 ? resolve() : reject()),
-          progressStream());
+          progressStream()
+        );
       }, reject));
   }
 };
